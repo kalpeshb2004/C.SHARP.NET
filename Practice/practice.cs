e4374a395874b5574d7080e5eef3b14929f3ef6a
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-09-02 10:23:37 +0530

    .net setup done

---
+++ Practice/practice.cs
@@ -0,0 +1,10 @@
+Console.Write("enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("enter a 2nd number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine("a is bigger");
+else
+    Console.WriteLine("b is bigger");
