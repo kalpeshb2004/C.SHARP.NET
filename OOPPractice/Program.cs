a657da246af62ab43d927ac00662fb0f569e8711
kalpeshb2004 <kalpeshbhure01@gmail.com>
2026-09-22 21:56:29 +0530

    all setup done

--- OOPPractice/Program.cs
+++ OOPPractice/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("hello guys weolcome to #");
+﻿using System;
+
+Car car1 = new Car("Honda");
+car1.Accelerate();
+
+class Car
+{
+    public string Brand;
+    public int Speed;
+
+    public Car(string brand)
+    {
+        Brand = brand;
+        Speed = 0;
+    }
+
+    public void Accelerate()
+    {
+        Speed += 10;
+        Console.WriteLine($"{Brand} speed: {Speed}");
+    }
+}
